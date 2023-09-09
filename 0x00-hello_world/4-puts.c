46b96643511ea1f84727cc97bb75396ecd158674
sanousi1 <adelsanousi@gmail.com>
2023-09-09 05:37:39 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
-#inclde<stdio.h>
+#inclde < stdio.h >
 
-int main()
+\**
+ * main - this is the main function
+ * return: always 0
+ */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
