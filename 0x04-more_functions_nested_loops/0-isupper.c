853812c17276c627ce739f1da23bbddd63ea3027
sanousi1 <adelsanousi@gmail.com>
2023-09-14 11:48:23 -0700

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,7 +3,7 @@
 /**
 * _isupper - this is the function of the task
 *@c : the function prarmeter
-* Return: 1 it it's uppercase
+* Return: 1 it it's uppercase, otherwise 0.
 */
 int _isupper(int c)
 {
