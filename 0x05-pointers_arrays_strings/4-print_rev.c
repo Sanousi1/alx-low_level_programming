58d2f13d923fdf1f26d51942543817273668d759
sanousi1 <adelsanousi@gmail.com>
2023-09-18 11:09:57 -0700

    print in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* print_rev - this is the function of the task
+*@s : the function prarmeter
+* Return:void
+*/
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+	while(i >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
+}
