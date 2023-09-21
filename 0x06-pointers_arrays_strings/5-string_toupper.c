b4645bcb173235010dba4a9c33c00adb842dd28d
sanousi1 <adelsanousi@gmail.com>
2023-09-21 10:04:48 -0700

    string toupper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* *string_toupper - this is the function of the task
+*@* : the function prarmeter
+* Return: return result
+*/
+char *string_toupper(char *)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <='z')
+			str[i] =str[i] - 32;
+	}
+	return (str);
+}
