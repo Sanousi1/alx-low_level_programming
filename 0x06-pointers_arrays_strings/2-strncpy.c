fba3eb2be4f89a291ffd399fc066da00384bd87f
sanousi1 <adelsanousi@gmail.com>
2023-09-21 09:25:46 -0700

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* *_strncpy - this is the function of the task
+*@dest : the function prarmeter
+*@src : the function prarmeter
+*@n : the function prarmeter
+* Return: return dest
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0' && i < n; i++)
+		dest[i] = src[i];
+	for (; i < n; i++);
+		dest[i] = '\0';
+	return (dest);
+}
