a05d95858dbde59aea23ff8ce0c8725e09cbb4a8
sanousi1 <adelsanousi@gmail.com>
2023-09-26 07:53:10 -0700

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* *_strchr - this is the function of the task
+*@s : the function prarmeter
+*@c : the function prarmeter
+* Return: return result
+*/
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	if (*s == c)
+		return (s);
+	return (NULL);
+}
